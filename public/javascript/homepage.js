fe1bcc29698bb30685e3d4de974e2237b546ea7a
Dan Lyons <danielclyons@yahoo.com>
2020-09-21 20:54:40 -0400

    make some buttons in cart non-functional

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -247,10 +247,10 @@ var shoppingCart = (function() {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
         + "<td>(" + cartArray[i].price + ")</td>"
-        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name= '" + cartArray[i].name + "'>-</button>"
-        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
-        + "<button class='plus-item btn btn-primary input-group-addon' data-name= '" + cartArray[i].name + "'>+</button></div></td>"
-        + "<td><button class='delete-item btn btn-danger' data-name='" + cartArray[i].name + "'>X</button></td>"
+        + "<td><div class='input-group'><div class='minus-item input-group-addon btn btn-primary' data-name= '" + cartArray[i].name + "'></div>"
+        + "<div type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "<div class='plus-item btn btn-primary input-group-addon' data-name= '" + cartArray[i].name + "'></div></div></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name='" + cartArray[i].name + "'></button></td>"
         + " = " 
         + "<td>" + cartArray[i].total + "</td>" 
         +  "</tr>";
@@ -262,33 +262,33 @@ var shoppingCart = (function() {
   
   // Delete item button
   
-  $('.show-cart').on("click", ".delete-item", function(event) {
-    var name = $(this).data('name')
-    shoppingCart.removeItemFromCartAll(name);
-    displayCart();
-  })
+  // $('.show-cart').on("click", ".delete-item", function(event) {
+  //   var name = $(this).data('name')
+  //   shoppingCart.removeItemFromCartAll(name);
+  //   displayCart();
+  // })
   
   
   // -1
-  $('.show-cart').on("click", ".minus-item", function(event) {
-    var name = $(this).data('name')
-    shoppingCart.removeItemFromCart(name);
-    displayCart();
-  })
+  // $('.show-cart').on("click", ".minus-item", function(event) {
+  //   var name = $(this).data('name')
+  //   shoppingCart.removeItemFromCart(name);
+  //   displayCart();
+  // })
   // +1
-  $('.show-cart').on("click", ".plus-item", function(event) {
-    var name = $(this).data('name')
-    shoppingCart.addItemToCart(name);
-    displayCart();
-  })
+  // $('.show-cart').on("click", ".plus-item", function(event) {
+  //   var name = $(this).data('name')
+  //   shoppingCart.addItemToCart(name);
+  //   displayCart();
+  // })
   
   // Item count input
-  $('.show-cart').on("change", ".item-count", function(event) {
-     var name = $(this).data('name');
-     var count = Number($(this).val());
-    shoppingCart.setCountForItem(name, count);
-    displayCart();
-  });
+  // $('.show-cart').on("change", ".item-count", function(event) {
+  //    var name = $(this).data('name');
+  //    var count = Number($(this).val());
+  //   shoppingCart.setCountForItem(name, count);
+  //   displayCart();
+  // });
   
   displayCart();
   
